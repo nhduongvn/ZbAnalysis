13f0ad6c7bd4f35274c34a434df615abe61e553c
Duong <nhduongvn@gmail.com>
2021-01-04 08:17:08 -0600

    emu control regions

--- src/Plots.cxx
+++ src/Plots.cxx
@@ -14,8 +14,10 @@ const unsigned NBIN_PT_Z = 300;
 const float X_PT_Z[2] = {0,300};
 const unsigned NBIN_DR_2B = 100;
 const float X_DR_2B[2] = {0,10};
-//const unsigned NBIN_DPHI_2B = 60;
-//const float X_DPHI_2B[2] = {0,TMath::Pi()};
+const unsigned NBIN_ETA = 300;
+const float X_ETA[2] = {-3,3};
+const unsigned NBIN_PHI = 120;
+const float X_PHI[2] = {-TMath::Pi(),TMath::Pi()};
 const unsigned NBIN_DRMIN_Z2B = 100;
 const float X_DRMIN_Z2B[2] = {0,10};
 const unsigned NBIN_A_Z2B = 100;
@@ -50,8 +52,8 @@ class ZbPlots
     ZbPlots(TString name) : m_name(name) {
       //FIXME add ptZ
       h_pt_lep0 = new TH1D(name + "_pt_lep0","",200,0,200) ;
-      h_phi_lep0 = new TH1D(name + "_phi_lep0","",400,-4.0,4.0) ;
-      h_eta_lep0 = new TH1D(name + "_eta_lep0","",300,-3.0,3.0) ;
+      h_phi_lep0 = new TH1D(name + "_phi_lep0","",NBIN_PHI,X_PHI[0],X_PHI[1]) ;
+      h_eta_lep0 = new TH1D(name + "_eta_lep0","",NBIN_ETA,X_ETA[0],X_ETA[1]) ;
       h_deltaRroot_lep0 = new TH1D(name + "_deltaRroot_lep0","",10000,-0.5,9.5) ;
       h_deltaPhiRoot_lep0 = new TH1D(name + "_deltaPhiRoot_lep0","",800,-4.0,4.0) ;
       h_deltaEtaRoot_lep0 = new TH1D(name + "_deltaEtaRoot_lep0","",800,-4.0,4.0) ;
@@ -69,8 +71,8 @@ class ZbPlots
       h_deltaEtaAlgo_lep0->Sumw2() ;
 
       h_pt_lep1 = new TH1D(name + "_pt_lep1","",200,0,200) ;
-      h_phi_lep1 = new TH1D(name + "_phi_lep1","",400,-4.0,4.0) ;
-      h_eta_lep1 = new TH1D(name + "_eta_lep1","",300,-3.0,3.0) ;
+      h_phi_lep1 = new TH1D(name + "_phi_lep1","",NBIN_PHI,X_PHI[0],X_PHI[1]) ;
+      h_eta_lep1 = new TH1D(name + "_eta_lep1","",NBIN_ETA,X_ETA[0],X_ETA[1]) ;
       h_ZMass = new TH1D(name + "_ZMass","",300,0,300) ;
       h_deltaRroot_lep1 = new TH1D(name + "_deltaRroot_lep1","",10000,-0.5,9.5) ;
       h_deltaPhiRoot_lep1 = new TH1D(name + "_deltaPhiRoot_lep1","",800,-4.0,4.0) ;
@@ -91,8 +93,8 @@ class ZbPlots
       h_deltaEtaAlgo_lep1->Sumw2() ;
 
       h_pt_jet = new TH1D(name + "_pt_jet","",NBIN_PT_JET,X_PT_JET[0],X_PT_JET[1]) ;
-      h_phi_jet = new TH1D(name + "_phi_jet","",400,-4.0,4.0) ;
-      h_eta_jet = new TH1D(name + "_eta_jet","",300,-3.0,3.0) ;
+      h_phi_jet = new TH1D(name + "_phi_jet","",NBIN_PHI,X_PHI[0],X_PHI[1]) ;
+      h_eta_jet = new TH1D(name + "_eta_jet","",NBIN_ETA,X_ETA[0],X_ETA[1]) ;
       h_mSV_jet = new TH1D(name + "_mSV_jet","",100,0,10) ;
       h_Njet = new TH1D(name + "_Njet","",15,0,15) ;
       h_pt_jet->Sumw2() ;
@@ -102,8 +104,8 @@ class ZbPlots
       h_mSV_jet->Sumw2();
 
       h_pt_dilepton = new TH1D(name + "_pt_dilepton","",NBIN_PT_Z,X_PT_Z[0],X_PT_Z[1]) ;
-      h_phi_dilepton = new TH1D(name + "_phi_dilepton","",400,-4.0,4.0) ;
-      h_eta_dilepton = new TH1D(name + "_eta_dilepton","",300,-3.0,3.0) ;
+      h_phi_dilepton = new TH1D(name + "_phi_dilepton","",NBIN_PHI,X_PHI[0],X_PHI[1]) ;
+      h_eta_dilepton = new TH1D(name + "_eta_dilepton","",NBIN_ETA,X_ETA[0],X_ETA[1]) ;
       h_pt_dilepton->Sumw2() ;
       h_phi_dilepton->Sumw2() ;
       h_eta_dilepton->Sumw2() ;
@@ -233,8 +235,8 @@ class Z2bPlots : public ZbPlots{
   public: 
     Z2bPlots(TString name) : ZbPlots(name) {
       h_pt_jet1 = new TH1D(name + "_pt_jet1","",NBIN_PT_JET,X_PT_JET[0],X_PT_JET[1]) ;
-      h_phi_jet1 = new TH1D(name + "_phi_jet1","",400,-4.0,4.0) ;
-      h_eta_jet1 = new TH1D(name + "_eta_jet1","",300,-3.0,3.0) ;
+      h_phi_jet1 = new TH1D(name + "_phi_jet1","",NBIN_PHI,X_PHI[0],X_PHI[1]) ;
+      h_eta_jet1 = new TH1D(name + "_eta_jet1","",NBIN_ETA,X_ETA[0],X_ETA[1]) ;
       h_mSV_jet1 = new TH1D(name + "_mSV_jet1","",100,0,10) ;
       h_m_2b = new TH1D(name + "_m_2b","",NBIN_M_2B,X_M_2B[0],X_M_2B[1]) ;
       h_m_Z2b = new TH1D(name + "_m_Z2b","",NBIN_M_Z2B,X_M_Z2B[0],X_M_Z2B[1]) ;
@@ -337,6 +339,100 @@ class Z2bPlots : public ZbPlots{
     TH1D* h_dRmax_Z2b ;
     TH1D* h_A_Z2b ;
     TH1D* h_pt_Z2b ;
+} ;
+
+class ElMu2bPlots {
+  
+  public: 
+    ElMu2bPlots(TString name) {
+
+      h_pt_ele = new TH1D(name + "_pt_ele","",200,0,200) ;
+      h_phi_ele = new TH1D(name + "_phi_ele","",NBIN_PHI,X_PHI[0],X_PHI[1]) ;
+      h_eta_ele = new TH1D(name + "_eta_ele","",NBIN_ETA,X_ETA[0],X_ETA[1]) ;
+      h_pt_muon = new TH1D(name + "_pt_muon","",200,0,200) ;
+      h_phi_muon = new TH1D(name + "_phi_muon","",NBIN_PHI,X_PHI[0],X_PHI[1]) ;
+      h_eta_muon = new TH1D(name + "_eta_muon","",NBIN_ETA,X_ETA[0],X_ETA[1]) ;
+      
+      h_Zmass = new TH1D(name + "_ZMass","",300,0,300) ;
+    
+      h_pt_jet = new TH1D(name + "_pt_jet","",NBIN_PT_JET,X_PT_JET[0],X_PT_JET[1]) ;
+      h_phi_jet = new TH1D(name + "_phi_jet","",NBIN_PHI,X_PHI[0],X_PHI[1]) ;
+      h_eta_jet = new TH1D(name + "_eta_jet","",NBIN_ETA,X_ETA[0],X_ETA[1]) ;
+
+      h_pt_jet1 = new TH1D(name + "_pt_jet1","",NBIN_PT_JET,X_PT_JET[0],X_PT_JET[1]) ;
+      h_phi_jet1 = new TH1D(name + "_phi_jet1","",NBIN_PHI,X_PHI[0],X_PHI[1]) ;
+      h_eta_jet1 = new TH1D(name + "_eta_jet1","",NBIN_ETA,X_ETA[0],X_ETA[1]) ;
+      
+      h_met = new TH1D(name + "_met","",500,0,500);
+      
+      h_pt_ele->Sumw2();
+      h_phi_ele->Sumw2();
+      h_eta_ele->Sumw2();
+      h_pt_muon->Sumw2();
+      h_phi_muon->Sumw2();
+      h_eta_muon->Sumw2();
+      h_Zmass->Sumw2();
+      h_pt_jet->Sumw2() ;
+      h_phi_jet->Sumw2() ;
+      h_eta_jet->Sumw2() ;
+      h_pt_jet1->Sumw2() ;
+      h_phi_jet1->Sumw2() ;
+      h_eta_jet1->Sumw2() ;
+      h_met->Sumw2();
+    } ;
+
+    void Fill(LepObj& ele, LepObj& muon, JetObj& J, JetObj& J1, float met, float w=1) {
+      h_pt_ele->Fill(ele.m_lvec.Pt(), w) ;
+      h_eta_ele->Fill(ele.m_lvec.Eta(), w) ;
+      h_phi_ele->Fill(ele.m_lvec.Phi(), w) ;
+      h_pt_muon->Fill(muon.m_lvec.Pt(), w) ;
+      h_eta_muon->Fill(muon.m_lvec.Eta(), w) ;
+      h_phi_muon->Fill(muon.m_lvec.Phi(), w) ;
+      h_Zmass->Fill((ele.m_lvec+muon.m_lvec).M(), w) ;
+      h_pt_jet->Fill(J.m_lvec.Pt(), w) ;
+      h_eta_jet->Fill(J.m_lvec.Eta(), w) ;
+      h_phi_jet->Fill(J.m_lvec.Phi(), w) ;
+      h_pt_jet1->Fill(J1.m_lvec.Pt(), w) ;
+      h_eta_jet1->Fill(J1.m_lvec.Eta(), w) ;
+      h_phi_jet1->Fill(J1.m_lvec.Phi(), w) ;
+      h_met->Fill(met,w);
+    } ;
+
+    std::vector<TH1*> returnHisto(){
+      std::vector<TH1*> histolist;
+      histolist.push_back(h_pt_ele);
+      histolist.push_back(h_phi_ele);
+      histolist.push_back(h_eta_ele);
+      histolist.push_back(h_pt_muon);
+      histolist.push_back(h_phi_muon);
+      histolist.push_back(h_eta_muon);
+      histolist.push_back(h_Zmass);
+      histolist.push_back(h_pt_jet) ;
+      histolist.push_back(h_phi_jet) ;
+      histolist.push_back(h_eta_jet) ;
+      histolist.push_back(h_pt_jet1) ;
+      histolist.push_back(h_phi_jet1) ;
+      histolist.push_back(h_eta_jet1) ;
+      histolist.push_back(h_met) ;
+
+      return histolist ;
+    }
+
+  protected:
+    TH1D* h_pt_ele;
+    TH1D* h_phi_ele;
+    TH1D* h_eta_ele;
+    TH1D* h_pt_muon;
+    TH1D* h_phi_muon;
+    TH1D* h_eta_muon;
+    TH1D* h_Zmass;
+    TH1D* h_pt_jet ;
+    TH1D* h_phi_jet ;
+    TH1D* h_eta_jet ;
+    TH1D* h_pt_jet1 ;
+    TH1D* h_phi_jet1 ;
+    TH1D* h_eta_jet1 ;
+    TH1D* h_met;
 } ;
 
 class EffPlots
